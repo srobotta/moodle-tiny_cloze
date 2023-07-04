d98bb3081b5aa00a6e048a2201ac5b34a8016a6a
Stephan Robotta <stephan.robotta@bfh.ch>
2023-07-04 12:02:57 +0200

    Prepare 1.0 release

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_cloze';
-$plugin->release = '0.1.0';
-$plugin->version = 2023061302;
+$plugin->release = '1.0';
+$plugin->version = 2023070400;
 $plugin->requires = 2022112800;
-$plugin->maturity = MATURITY_ALPHA;
+$plugin->maturity = MATURITY_RC;
